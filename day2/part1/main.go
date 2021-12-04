e6beb532be6f95b48fd8b9567f5c09858035b278
Daniel Pendolino <dpendolino@gmail.com>
2021-12-04 12:40:32 -0500

    day2 part1

---
+++ day2/part1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	// file, err := os.Open("sample.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	depth, position := 0, 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		words := strings.Fields(scanner.Text())
+		direction := words[0]
+		magnitude, _ := strconv.Atoi(words[1])
+		switch direction {
+		case "forward":
+			position += magnitude
+		case "backward":
+			position -= magnitude
+		case "up":
+			depth -= magnitude
+		case "down":
+			depth += magnitude
+		}
+	}
+
+	fmt.Printf("%v\n", depth)
+	fmt.Printf("%v\n", position)
+	fmt.Printf("%v\n", position*depth)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
